6644ef04d6535b4742d201e2c589b5b1d92f10e5
Jayraj Roshan <jayrajroshan1@gmail.com>
2020-07-28 11:42:03 +0530

    changed sensor 1 to 2

--- index.js
+++ index.js
@@ -79,9 +79,24 @@ const gets1 = (request, response) => {
     )
 }
 
+const gets2 = (request, response) => {
+    pool.query(
+        'SELECT * FROM sensordata21 ORDER BY serial_no DESC LIMIT 10',
+        (error, results) => {
+            if (error) {
+                throw error
+            }
+
+            response.status(200).json(results.rows)
+        },
+
+    )
+}
+
 
 
 app.get('/s3', gets3)
+app.get('/s2', gets2)
 app.get('/s1', gets1)
 
 
